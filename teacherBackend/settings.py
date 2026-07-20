81cedd2be4db59daa2aa69d50a090c6f1a58473d
Shivaram Sagar <sagarshivaram44@gmail.com>
2026-07-21 00:07:00 +0530

    Update email backend to Brevo HTTP API

--- teacherBackend/settings.py
+++ teacherBackend/settings.py
@@ -204,7 +204,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 AUTH_USER_MODEL = 'apis.User'
 
 # Email Settings for real email sending
-EMAIL_BACKEND = os.getenv('EMAIL_BACKEND', 'apis.email_backend.ResendHTTPEmailBackend')
+EMAIL_BACKEND = os.getenv('EMAIL_BACKEND', 'apis.email_backend.BrevoHTTPEmailBackend')
 EMAIL_HOST = os.getenv('EMAIL_HOST', 'smtp.gmail.com')
 EMAIL_PORT = int(os.getenv('EMAIL_PORT', 587))
 
